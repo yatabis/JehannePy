a6f055b2e9533140e4601a64ac8c96447c79cbf8
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-08-22 23:11:32 +0900

    test: webhookの応答のテストです

--- jehanne.py
+++ jehanne.py
@@ -10,11 +10,12 @@ url_reply = "https://api.line.me/v2/bot/message/push"
 
 @route("/callback", method='POST')
 def callback():
-    headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
-    body = json.dumps({'to': MASTER,
-                       'messages': [{'type': 'text', 'text': "リクエストがPOSTされました。"}]})
-    req = requests.post(url_reply, data=body, headers=headers)
-    result = "request success" if req.status_code == 200 else f"Error: {req.status_code}\nDetail: {req.content}"
+    # headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
+    # body = json.dumps({'to': MASTER,
+    #                    'messages': [{'type': 'text', 'text': "リクエストがPOSTされました。"}]})
+    # req = requests.post(url_reply, data=body, headers=headers)
+    result = 200
+    # "request success" if req.status_code == 200 else f"Error: {req.status_code}\nDetail: {req.content}"
     return f"Hi, this is Jehanne.\n{result}"
 
 

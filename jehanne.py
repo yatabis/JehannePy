28d29062add459c7a8ef06b485b1ac0e08056fa7
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-08-22 23:36:48 +0900

    test: webhookの応答のテストです

--- jehanne.py
+++ jehanne.py
@@ -6,19 +6,22 @@ from bottle import route, run, request
 
 CAT = os.environ["CHANNEL_ACCESS_TOKEN"]
 MASTER = os.environ["MASTER"]
-url_reply = "https://api.line.me/v2/bot/message/push"
+url_reply = "https://api.line.me/v2/bot/message/reply"
 
 
 @route("/callback", method='POST')
 def callback():
-    # headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
-    # body = json.dumps({'to': MASTER,
-    #                    'messages': [{'type': 'text', 'text': "リクエストがPOSTされました。"}]})
-    # req = requests.post(url_reply, data=body, headers=headers)
     events = json.loads(request.forms.body)
     for event in events['events']:
-        print(event)
-    return f"Hi, this is Jehanne.\n{events}"
+        if event['type'] == "message":
+            reply_token = event['replyToken']
+            mes_type = event['message']['type']
+            if mes_type == "text":
+                headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
+                body = json.dumps({'replyToken': reply_token,
+                                   'messages': [{'type': 'text', 'text': "リクエストがPOSTされました。"}]})
+                req = requests.post(url_reply, data=body, headers=headers)
+    return f"Hi, this is Jehanne.\n{req}"
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 443)))

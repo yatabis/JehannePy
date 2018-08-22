020a0489d64aa74a2e616bc95be41732061db3a7
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-08-22 22:36:45 +0900

    test: 応答トークンのテストです

--- jehanne.py
+++ jehanne.py
@@ -1,10 +1,21 @@
 import os
-from bottle import route, run
+import requests
+from bottle import route, run, request
+
+
+CAT = os.environ["CHANNEL_ACCESS_TOKEN"]
+MASTER = os.environ["MASTER"]
+url_reply = "https://api.line.me/v2/bot/message/push"
 
 
 @route("/callback", method='POST')
 def callback():
-    return "callback: Hi, this is Jehanne."
+    headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
+    body = json.dumps({'to': MASTER,
+                       'messages': [{'type': 'text', 'text': "リクエストがPOSTされました。"}]})
+    req = requests.post(url_LINE, data=body, headers=headers)
+    result = "request success" if req.status_code == 200 else f"Error: {req.status_code}\nDetail: {req.content}"
+    return f"Hi, this is Jehanne.\n{result}"
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 443)))

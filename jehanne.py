24aec8e9e18b0c8910a2c9d0f903b45e33c765ce
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-08-26 23:15:54 +0900

    update: スタンプの返答に対応しました。

--- jehanne.py
+++ jehanne.py
@@ -30,8 +30,8 @@ def callback():
         elif message.type == "audio":
             message.reply_text("音声を受け取りました。")
         elif message.type == "sticker":
-            message.reply_text(f"スタンプを受け取りました。{message.message}")
-            if message.message[0] in range(1, 5):
+            message.reply_text("スタンプを受け取りました。")
+            if int(message.message[0]) in range(1, 5):
                 message.reply_sticker(*message.message)
             else:
                 message.reply_text("こちらから送信できないスタンプです。")

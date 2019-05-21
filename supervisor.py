8f99ce96d7c8ac2180d6f2bae007e0fda27ea3c1
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-05-21 23:09:52 +0900

    fix: problem to chage state of Jehanne to crashed when mstdn server raise 502 error

--- supervisor.py
+++ supervisor.py
@@ -4,20 +4,25 @@ import LINEbot
 
 BASE_URL = "https://api.heroku.com/apps/jehanne/dynos"
 DYNO = "worker.1"
+EP = f"{BASE_URL}/{DYNO}"
 API_KEY = os.environ['API_KEY']
 HEADER = {"Content-Type": "application/json",
           "Accept": "application/vnd.heroku+json; version=3",
           "Authorization": f"Bearer {API_KEY}"}
 
 bot = LINEbot.LineMessage()
-req = requests.get(f"{BASE_URL}/{DYNO}", headers=HEADER)
+req = requests.get(EP, headers=HEADER)
 if req.status_code == 200:
     state = req.json()['state']
     if not state == 'up':
         bot.push_text(f"【supervisor】\nJehanneのstateが{state}になっていました。")
-
-    req = requests.get("https://jehanne.herokuapp.com/ping")
-    if not req.status_code == 200:
-        bot.push_text(f"【supervisor】\nJehanneのstate変更に失敗しました。\nstatus code: {req.status_code}")
+    if state in ['up', 'idle']:
+        req = requests.get("https://jehanne.herokuapp.com/ping")
+        if not req.status_code == 200:
+            bot.push_text(f"【supervisor】\nJehanneへのpingの送信に失敗しました。\nstatus code: {req.status_code}")
+    elif state == 'crashed':
+        req = requests.delete(EP, headers=HEADER)
+        if not req.status_code == 200:
+            bot.push_text(f"【supervisor】\nJehanneのstate変更に失敗しました。\nstatus code: {req.status_code}")
 else:
     bot.push_text(f"【supervisor】\nJehanneのstate取得に失敗しました。\nstatus code: {req.status_code}")

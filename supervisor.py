5f9dadc6216efb921f5bf9a206defec252a53035
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-05-21 23:19:28 +0900

    fix: problem to chage state of Jehanne to crashed when mstdn server raise 502 error

--- supervisor.py
+++ supervisor.py
@@ -22,7 +22,7 @@ if req.status_code == 200:
             bot.push_text(f"【supervisor】\nJehanneへのpingの送信に失敗しました。\nstatus code: {req.status_code}")
     elif state == 'crashed':
         req = requests.delete(EP, headers=HEADER)
-        if not req.status_code == 200:
+        if not req.status_code == 202:
             bot.push_text(f"【supervisor】\nJehanneのstate変更に失敗しました。\nstatus code: {req.status_code}")
 else:
     bot.push_text(f"【supervisor】\nJehanneのstate取得に失敗しました。\nstatus code: {req.status_code}")
